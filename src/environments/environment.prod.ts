8813608e7b75dc74946dc59795b47be6e1a7da26
Edward Opare-Aryee <ed@thisissoon.com>
2017-10-12 18:02:23 +0100

    fix redirect url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   playerAuthHeader: 'Access-Token',
   playerSocketUrl: 'https://ws.thisissoon.fm/',
   googleClientId: '999522755620-kiiv5l08jf9ummob1iqveokd373r3r60.apps.googleusercontent.com',
-  googleRedirectUri: 'https://thisissoon.fm/',
+  googleRedirectUri: 'https://new.thisissoon.fm/',
   googleAuthTokenPrefix: 'sn_fm',
   googleAuthTokenName: 'access_token',
   spotifyMarket: 'GB',
