7176ad3647f03d275fbdadaf963e0150a54dc502
Edward Opare-Aryee <ed@thisissoon.com>
2017-10-31 15:18:13 +0000

    added tests for app component

--- src/app/shared/animations/nav-fade.animation.ts
+++ src/app/shared/animations/nav-fade.animation.ts
@@ -8,15 +8,15 @@ export const navFadeAnimation = trigger('navFade', [
   state('in', style({ opacity: 1 })),
   state('out', style({ opacity: 0, display: 'none' })),
   transition('* => in', [
-    query('.nav-item-animate', style({ opacity: 0 })),
+    query('.nav-item-animate', style({ opacity: 0 }), { optional: true }),
     style({ opacity: 1, display: 'block' }),
     query('.nav-item-animate', stagger('200ms', [
       animate('350ms .5s ease-out', style({opacity: 1}))
-    ]))
+    ]), { optional: true })
   ]),
   transition('* => out', [
     query('.nav-item-animate', stagger('200ms', [
       animate('350ms ease-out', style({opacity: 0}))
-    ]))
+    ]), { optional: true })
   ])
 ]);
